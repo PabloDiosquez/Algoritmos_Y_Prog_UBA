ce39d49f6eb21aaca16308b7e803de63bd047b78
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-08-28 20:08:33 -0300

    Se agrega en 05-Ciclos, el ejercicios _02.py

--- 05-Ciclos/_02.py
+++ 05-Ciclos/_02.py
@@ -1,6 +1,16 @@
 # Escribir una función que reciba un número entero k e imprima su descomposición
 # en factores primos.
 
+from helpers import es_divisible, es_primo, obtener_exponente_en_factorizacion
+
 def imprimir_factorización_primos(n: int) -> None:
     "Dado un número, imprime su factorización en primos."
-    ...
+    factorizacion = []
+    for i in range(2, (n//2) + 1):
+        if es_primo(i) and es_divisible(n, i):
+            factorizacion.append(f'{i}^{obtener_exponente_en_factorizacion(n, i)}')
+    factorizacion = 'x'.join(factorizacion)
+    
+    print(f'{n} = {factorizacion}') 
+
+imprimir_factorización_primos(75) 
