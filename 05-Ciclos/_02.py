740f638a9e31e28efa62f1beea16afe675c79f4a
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-08-28 20:12:22 -0300

    Se agrega en 05-Ciclos, el ejercicios _02.py

--- 05-Ciclos/_02.py
+++ 05-Ciclos/_02.py
@@ -3,14 +3,26 @@
 
 from helpers import es_divisible, es_primo, obtener_exponente_en_factorizacion
 
-def imprimir_factorización_primos(n: int) -> None:
-    "Dado un número, imprime su factorización en primos."
-    factorizacion = []
-    for i in range(2, (n//2) + 1):
-        if es_primo(i) and es_divisible(n, i):
-            factorizacion.append(f'{i}^{obtener_exponente_en_factorizacion(n, i)}')
-    factorizacion = 'x'.join(factorizacion)
-    
-    print(f'{n} = {factorizacion}') 
+def imprimir_factorizacion_primos(numero: int) -> None:
+    """
+    Dado un número, imprime su factorización en primos.
 
-imprimir_factorización_primos(75) 
+    Si el número es menor o igual a 1, o si es primo, se imprime el número tal cual.
+    De lo contrario, se imprime la factorización en primos del número.
+    
+    Parameters:
+    numero (int): El número que se desea factorizar.
+    """
+
+    if numero <= 1 or es_primo(numero):
+        print(f'{numero} = {numero}')
+        return
+    
+    factorizacion = []
+    for i in range(2, (numero // 2) + 1):
+        if es_primo(i) and es_divisible(numero, i):
+            exponente = obtener_exponente_en_factorizacion(numero, i)
+            factorizacion.append(f'{i}^{exponente}')
+    
+    factorizacion_str = ' x '.join(factorizacion)
+    print(f'{numero} = {factorizacion_str}')
