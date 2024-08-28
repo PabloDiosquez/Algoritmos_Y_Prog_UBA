334f89dffc2fe43134545a6da5f2c763f7299094
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-08-28 19:56:54 -0300

    Se agrega en 05-Ciclos, el ejercicios _02.py

--- 05-Ciclos/helpérs.py
+++ 05-Ciclos/helpérs.py
@@ -1,27 +1,59 @@
-def es_divisible(n: int, k: int) -> bool:
-    """Indica si el primer número es divisible por el segundo.
-    Lanza ValueError si se intenta dividir por 0.
+def es_divisible(dividendo: int, divisor: int) -> bool:
     """
-    if not k:
+    Indica si el primer número (dividendo) es divisible por el segundo (divisor).
+    
+    Lanza ValueError si se intenta dividir por 0.
+    
+    Parameters:
+    dividendo (int): El número que se desea dividir.
+    divisor (int): El número por el cual se desea dividir.
+    
+    Returns:
+    bool: True si el dividendo es divisible por el divisor, False en caso contrario.
+    """
+    if divisor == 0:
         raise ValueError('No se puede dividir por 0.')
-    return not n % k
+    return dividendo % divisor == 0
 
-def es_primo(n: int) -> bool:
-    "Indica si el número dado es primo."
-    return n > 1 and not tiene_divisores_propios(n)
+def es_primo(numero: int) -> bool:
+    """
+    Indica si el número dado es un número primo.
+    
+    Parameters:
+    numero (int): El número que se desea evaluar.
+    
+    Returns:
+    bool: True si el número es primo, False en caso contrario.
+    """
+    return numero > 1 and not tiene_divisores_propios(numero)
 
-def tiene_divisores_propios(n: int) -> bool:
-    "Indica si el número dado tiene divisores en el intervalo [2, n//2]"
-    for i in range(2, (n//2) + 1):
-        if es_divisible(n, i):
+def tiene_divisores_propios(numero: int) -> bool:
+    """
+    Indica si el número dado tiene divisores propios en el intervalo [2, numero//2].
+    
+    Parameters:
+    numero (int): El número que se desea evaluar.
+    
+    Returns:
+    bool: True si el número tiene divisores propios, False en caso contrario.
+    """
+    for i in range(2, (numero // 2) + 1):
+        if es_divisible(numero, i):
             return True
     return False
 
-
-def obtener_exponente_en_factorizacion(n: int, k: int) -> int:
+def obtener_exponente_en_factorizacion(numero: int, divisor: int) -> int:
     """
-    Devuelve el exponente del divisor en la factorización del primer número. Si el 'k' no divide a 'n' devuelve 0.
+    Devuelve el exponente del divisor en la factorización del primer número.
+    Si el divisor no divide al número, devuelve 0.
+    
+    Parameters:
+    numero (int): El número que se desea factorizar.
+    divisor (int): El divisor para el cual se desea encontrar el exponente.
+    
+    Returns:
+    int: El exponente del divisor en la factorización del número.
     """
-    if not es_divisible(n, k):
+    if not es_divisible(numero, divisor):
         return 0
-    return 1 + obtener_exponente_en_factorizacion(n // k, k)
+    return 1 + obtener_exponente_en_factorizacion(numero // divisor, divisor)
