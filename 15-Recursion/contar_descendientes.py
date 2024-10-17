685131da31a67db0ce4fb330c7d8570c64158c53
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-10-16 22:45:26 -0300

    Se agrega 'contar_descendientes.py'

--- 15-Recursion/contar_descendientes.py
+++ 15-Recursion/contar_descendientes.py
@@ -1,15 +1,23 @@
 # Existiendo la clase Persona, que posee el método obtener_hijos() -> List[Persona], se pide implementar una función contar_descendientes(persona: Persona) -> int que cuente cuántos descendientes tiene dicha persona (es decir, hijos + nietos + bisnietos + etc), utilizando recursión.
 
 class Persona:
-    def __init__(self):
-        pass
+    def obtener_hijos(self):
+        """
+        Retorna la lista de hijos de la persona.
+        """
+        pass  # Implementación para obtener los hijos
 
-    def obtener_hijos(self) -> list[Persona]:
-        pass
 
-    
 def contar_descendientes(persona: Persona) -> int:
-    '''
-    Recibe una persona y devuelve la cantidad de descendientes que tiene la misma.
-    '''
-    pass 
+    """
+    Cuenta el número total de descendientes de la persona dada, incluyendo hijos, 
+    nietos, bisnietos, etc., de forma recursiva.
+    """
+    if not persona.obtener_hijos():
+        return 0
+    
+    total_descendientes = 0
+    for hijo in persona.obtener_hijos():
+        total_descendientes += 1 + contar_descendientes(hijo)
+    
+    return total_descendientes
