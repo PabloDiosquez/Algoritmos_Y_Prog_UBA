fc46b694328a3f27870f802ab56c0963fbba01f1
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-10-21 14:23:31 -0300

    Se agrega 'area.c'

---
+++ 17-C/area.c
@@ -0,0 +1,13 @@
+// Escribir una función que permita calcular el área de un rectángulo dada su base
+// y altura.
+
+/**
+ * Calcula el área de un rectángulo dada su base y altura.
+ *
+ * @param base La longitud de la base del rectángulo.
+ * @param altura La longitud de la altura del rectángulo.
+ * @return El área del rectángulo (base * altura).
+ */
+float area_rectangulo(float base, float altura) {
+    return base * altura;
+}
