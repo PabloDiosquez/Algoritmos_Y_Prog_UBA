1a8b87a43e69d2b93f91897fc2134e8bda641c2a
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-10-23 18:45:52 -0300

    Se agrega 'busqueda_bin.c'

---
+++ 17-C/busqueda_bin.c
@@ -0,0 +1,15 @@
+// (búsqueda binaria) Implemente una función similar de búsqueda, utilizando búsqueda binaria.
+// Prototipos:
+
+// const int * busqueda_binaria(const int * v, /* vector donde buscar */
+//                              size_t n,      /* largo del vector */
+//                              int objetivo,  /* elemento a buscar */
+//                              size_t izq,    /* franja del vector donde buscar, */
+//                              size_t der);   /* delimitada por izq y der */
+
+// bool busqueda_binaria(int * v, /* vector donde buscar */
+//                        size_t n,      /* largo del vector */
+//                        int objetivo,  /* elemento a buscar */
+//                        size_t izq,    /* franja del vector donde buscar, */
+//                        size_t der,    /* delimitada por izq y der */
+//                        int ** resultado);
