10c4d9b02bc66c51d26b4cc7a1ba262dff5bdabe
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-10-28 15:07:54 -0300

    Se actualiza 'string.c'

--- 17-C/string.c
+++ 17-C/string.c
@@ -24,6 +24,27 @@ size_t strlen(const char *s) {
 
 // char * strcpy(char * dest, const char * orig);
 
+/**
+ * @brief Copia una cadena de caracteres de origen en una cadena de destino.
+ * 
+ * Esta función implementa una versión personalizada de la función estándar `strcpy` de la biblioteca `<string.h>`.
+ * Copia cada carácter de la cadena `orig` a la cadena `dest`, incluyendo el carácter nulo (`'\0'`) al final.
+ * La cadena `dest` debe tener suficiente espacio para contener la cadena `orig`.
+ * 
+ * @param dest Puntero al arreglo de destino donde se copiará la cadena.
+ * @param orig Puntero a la cadena de caracteres de origen.
+ * @return char* Retorna un puntero a la cadena de destino `dest`.
+ */
+char *strcpy(char *dest, const char *orig) {
+    size_t i;
+    for (i = 0; orig[i] != '\0'; i++) {
+        dest[i] = orig[i];
+    }
+    dest[i] = '\0'; // Agrega el carácter nulo al final de la cadena copiada
+    return dest;
+}
+
+
 // char * strncpy(char * dest, const char * orig, size_t n);
 
 // int strcmp(const char * s1, const char * s2);
