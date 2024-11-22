cd5970e82442a5c3a55ff8b283bf768ff132383d
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-11-22 09:27:48 -0300

    Se actualiza 'cadena.c'

--- 17-C/tda/cadena.c
+++ 17-C/tda/cadena.c
@@ -1,16 +1,42 @@
 // Implemente el TDA cadena, con las siguientes primitivas:
 
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct cadena{
-    char *s;
+
+typedef struct cadena {
+    char *str; 
 } cadena_t;
 
-cadena_t *cadena_crear(const char *s){
-    
+/**
+ * @brief Crea una nueva instancia de cadena_t con una copia de la cadena proporcionada.
+ *
+ * @param s Puntero a una cadena terminada en nulo.
+ * @return Puntero a una estructura cadena_t con la cadena copiada, o NULL si falla.
+ */
+cadena_t *cadena_crear(const char *s) {
+    if (s == NULL) {
+        return NULL;
+    }
+
+    cadena_t *cadena = malloc(sizeof(cadena_t));
+    if (cadena == NULL) {
+        return NULL; 
+    }
+
+    size_t len = strlen(s);
+    char *str = malloc(len + 1); 
+    if (str == NULL) {
+        free(cadena); 
+        return NULL;
+    }
+
+    strcpy(str, s);
+
+    cadena->str = str;
+    return cadena;
 }
 
-// cadena_t * cadena_crear(const char *);
 
 // void cadena_destruir(cadena_t *);
 
