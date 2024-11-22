b003b3d295d62240ab63aee51e6e41ef745be897
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-11-22 09:11:43 -0300

    Se actualiza 'cadena.c'

---
+++ 17-C/tda/cadena.c
@@ -0,0 +1,25 @@
+// Implemente el TDA cadena, con las siguientes primitivas:
+
+#include <stdlib.h>
+
+typedef struct cadena{
+    char *s;
+} cadena_t;
+
+cadena_t *cadena_crear(const char *s){
+    
+}
+
+// cadena_t * cadena_crear(const char *);
+
+// void cadena_destruir(cadena_t *);
+
+// cadena_t * cadena_copiar(cadena_t *, const cadena_t *);
+
+// cadena_t * cadena_clonar(const cadena_t *);
+
+// cadena_t * cadena_concatenar(cadena_t *, const cadena_t *);
+
+// cadena_t * cadena_strcpy(cadena_t *, const char *);
+
+// cadena_t * cadena_strcat(cadena_t *, const char *);
